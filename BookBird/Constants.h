5e0aa4e7973ea85c661678ffde7a79020ace4a2c
Rob Stearn <robstearn@me.com>
2012-10-27 13:36:48 +0100

    Created a top level constants file to hold #define statements for the API keys and URLs

---
+++ BookBird/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  BookBird
+//
+//  Created by Rob Stearn on 27/10/2012.
+//  Copyright (c) 2012 HackingWithRobAndSally. All rights reserved.
+//
+
+#define kSandboxAPIKey  @"e033f7e12cc4b18255d09bef5196fd4b"
+
+#define kBooksList      @"https://api.pearson.com/penguin/classics/v1/books"
+#define kChapterSearch  @"https://api.pearson.com/penguin/classics/v1/articles/"
