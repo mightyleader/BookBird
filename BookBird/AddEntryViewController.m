1766cec188662e8520fb321fed7be7702b771e50
Sally Shepard <sally@linesofcocoa.co.uk>
2012-10-28 13:04:38 +0000

    added add entry vc

---
+++ BookBird/AddEntryViewController.m
@@ -0,0 +1,40 @@
+//
+//  AddEntryViewController.m
+//  BookBird
+//
+//  Created by Sally on 28/10/2012.
+//  Copyright (c) 2012 HackingWithRobAndSally. All rights reserved.
+//
+
+#import "AddEntryViewController.h"
+
+@interface AddEntryViewController ()
+
+@end
+
+@implementation AddEntryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(id)sender {
+}
+@end
